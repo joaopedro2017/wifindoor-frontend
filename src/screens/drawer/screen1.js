bfb3cca2b99bde874e21d89185f5424606f30026
JoaoPresto <jguedespresto@gmail.com>
2020-10-22 11:37:21 -0300

    Create screen initials

---
+++ src/screens/drawer/screen1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { styles } from '../../styles/styles'
+
+Screen1 = () =>
+    <View style={styles.center}>
+        <Text style={styles.title}>Screen1</Text>
+    </View>
+
+export default Screen1;
---
+++ src/screens/drawer/screen2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { styles } from '../../styles/styles'
+
+Screen2 = () =>
+    <View style={styles.center}>
+        <Text style={styles.title}>Screen2</Text>
+    </View>
+
+export default Screen2;
